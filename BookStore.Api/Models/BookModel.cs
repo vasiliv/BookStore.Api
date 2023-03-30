1f4b084d583ac6fb3dfd137abc8740c88f1c0494
vasiliv <32122468+vasiliv@users.noreply.github.com>
2023-03-30 11:34:46 +0400

    Part 95 finished

--- BookStore.Api/Models/BookModel.cs
+++ BookStore.Api/Models/BookModel.cs
@@ -1,8 +1,11 @@
-﻿namespace BookStore.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Api.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please add title")]
         public string Title { get; set; }
         public string Description { get; set; }
     }
